5bde20dd40ceef03893686a5712dd514ee3f625f
Javed Khan <khanjaved.sk19@gmail.com>
2024-03-14 15:20:41 +0530

    Initial commit

---
+++ src/components/InputArea.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function InputArea(props) {
+  const [inputText, setInputText] = useState("");
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  return (
+    <div className="form">
+      <input
+        onChange={handleChange}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            props.onAdd(inputText);
+            setInputText("");
+          }
+        }}
+        type="text"
+        value={inputText}
+      />
+      <button
+        onClick={() => {
+          props.onAdd(inputText);
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
+}
+
+export default InputArea;
